b6440798e2530435b0a457db145cb690d131f0ba
ARVO\andrew.koltyakov <andrew.koltyakov@arvosys.com>
2019-07-25 18:13:46 +0300

    SharePoint Data Model

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { fetchAllMvps } from './api';
 import { flattify } from './map';
 import { dumpOnDisk } from './utils';
+import { dumpToSP } from './sp';
 
 fetchAllMvps([], 1, (p) => console.log(`Fetching page: ${p}`))
   .then(dumpOnDisk)
   .then(flattify)
   .then(dumpOnDisk)
+  .then(dumpToSP)
   .catch((error) => console.error(error));
